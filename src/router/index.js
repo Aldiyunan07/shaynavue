b0e3759cc389080b0a1372fb19c1dec53a42b2f1
Aldiyunan07 <Aldiyunan07@gmail.com>
2022-08-10 22:09:10 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ProductView from '../views/ProductView.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import SuccesView from '../views/SuccesView.vue'
+
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: ProductView
+  },
+  {
+    path: '/cart',
+    name: 'shoppingcart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'succesview',
+    component: SuccesView
+  }
+
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroSection />  
+    <WomanShayna /> 
+    <InstaShayna />
+    <PartnerShayna/>
+    <FooterShayna/>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  // import HelloWorld from '@/components/HelloWorld.vue'
+  import HeaderShayna from '@/components/HeaderShayna.vue'
+  import HeroSection from '@/components/HeroSection'
+  import WomanShayna from '@/components/WomanShayna.vue'
+  import InstaShayna from '@/components/InstaShayna.vue'
+  import PartnerShayna from '@/components/PartnerShayna.vue'
+  import FooterShayna from '@/components/FooterShayna.vue'
+  export default {
+    name: 'HomeView',
+    components: {
+    HeaderShayna,
+    HeroSection,
+    WomanShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+}
+  }
+</script>
