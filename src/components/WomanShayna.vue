4f5ff2a369d0fd92f04d910607e4d7952252fef3
Aldiyunan07 <Aldiyunan07@gmail.com>
2022-09-07 21:37:05 +0700

    Bab Integrasi API

--- src/components/WomanShayna.vue
+++ src/components/WomanShayna.vue
@@ -2,11 +2,11 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
                     <Carousel class="product-slider" :items="3" :dots="false" :nav="false" :autoplay="true" >
-                        <div class="product-item">
+                        <div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
                             <div class="pi-pic">
-                                <img src="img/mickey1.jpg" alt="" />
+                                <img v-bind:src="itemProduct.gallery[0].photo" alt="" />
                                 <ul>
                                     <li class="w-icon active">
                                         <a href="#"><i class="icon_bag_alt"></i></a>
@@ -19,80 +19,20 @@
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
+                                <div class="catagory-name"> {{ itemProduct.type }} </div>
                                 <route-link to="/product">
-                                        <h5>Mickey Baggy</h5>
+                                        <h5>{{ itemProduct.name }}</h5>
                                 </route-link>
                                 <div class="product-price">
-                                    $14.00
+                                    ${{ itemProduct.price }}
                                     <span>$35.00</span>
                                 </div>
                             </div>
                         </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
                     </Carousel>
+                </div>
+                <div class="col-lg-12" v-else>
+                    <p> Produk baru tidak tersedia untuk saat ini </p>
                 </div>
             </div>
         </div>
@@ -100,13 +40,25 @@
 </template>
 
 <script>
+import axios from "axios";
 import Carousel from 'vue-owl-carousel';
 export default {
     name : "WomanShayna",
     components: {
         Carousel
+    },
+    data() {
+        return {
+            products : []
+        };
+    },
+    mounted(){
+        axios
+        .get("http://localhost:8000/api/products")
+        .then(res => (this.products = res.data.meta.data.data))
+        .catch(err => console.log(err));
     }
-}
+};
 </script>
 <style scoped>
 .product-item{
